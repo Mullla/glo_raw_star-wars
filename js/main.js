de91cd3a570ce918866c321b52c7fcbcf97d233a
Mars <diablo_13_06@mail.ru>
2021-07-25 19:11:06 +0300

    remove: comments

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$( document ).ready(function() {
+$(document).ready(function () {
   const overlay = $(".overlay");
   const demo = $(".demo-modal");
   const demoBtns = $(".demo-btn");
@@ -7,34 +7,34 @@ $( document ).ready(function() {
   overlay.hide();
   demo.hide();
 
-  demoBtns.on('click',function(){
+  demoBtns.on("click", function () {
     overlay.show();
     demo.show();
   });
 
-  modalBtn.on('click', function(){
+  modalBtn.on("click", function () {
     overlay.hide();
     demo.hide();
   });
-  
-  $('.owl-carousel').owlCarousel({
-    loop:true, //бесконечная прокрутка
-    margin:10,
+
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
     navClass: ["slider__nav--left", "slider__nav--right"],
-    nav:true, 
-    responsive:{ //для адавтива: размер экрана
-        0:{
-            items:1
-        },
-        700:{
-            items:2
-        },
-        900:{
-            items:3
-        },
-        1200: {
-            items:3
-        }
-    }
-});
+    nav: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      700: {
+        items: 2,
+      },
+      900: {
+        items: 3,
+      },
+      1200: {
+        items: 3,
+      },
+    },
+  });
 });
